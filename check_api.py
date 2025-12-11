85cd84ff88f7db75c42a459d2347e01c3964033b
KMicha <michael.kahle@yahoo.de>
2025-12-11 15:36:33 +0100

    Microsoft-Translator-3

--- check_api.py
+++ check_api.py
@@ -294,10 +294,12 @@ def inqRapidMicroTranslate3(results=[]):
             results.append(":no_entry: **Not** subscribed to Microsoft-Translator-3")
             addSubscribeMessageToResults(results, "Microsoft-Translator-3", "https://rapidapi.com/apiship-apiship-default/api/microsoft-translator-text-api3")
             return False
+          print(jsonData['message'])
+        if('messages' in jsonData):
           if('The API is unreachable, please contact the API provider'==jsonData['message']):
             results.append(":no_entry: Microsoft-Translator-3 is unreachable")
             return False
-          print(jsonData['message'])
+          print(jsonData['messages'])
         if (len(jsonData)>0):
           results.append(":white_check_mark: Microsoft-Translator-3 status fine")
           print(jsonData)
